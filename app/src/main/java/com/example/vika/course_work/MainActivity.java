385f5f24f4502bcbb7fa97b141ca403cb838790d
VikaKhusti <viktorijahusti@gmail.com>
2019-04-29 20:17:30 +0300

    add but., and create act.

--- app/src/main/java/com/example/vika/course_work/MainActivity.java
+++ app/src/main/java/com/example/vika/course_work/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.vika.course_work;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button log_in, sign_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        log_in =  (Button) findViewById(R.id.logInBtn);
+        sign_up = (Button) findViewById(R.id.signUpBtn);
     }
 }

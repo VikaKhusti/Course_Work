3a2976c67ab0c6bb71682dc6fa3731b4cac05109
VikaKhusti <viktorijahusti@gmail.com>
2019-04-29 22:39:50 +0300

    user can be added to DB

--- app/src/main/java/com/example/vika/course_work/DBHelper.java
+++ app/src/main/java/com/example/vika/course_work/DBHelper.java
@@ -6,31 +6,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "myDB";
-    public static final String TABLE_USERS = "users";
 
-    public static final String KEY_ID = "_id";
-    public static final String KEY_NAME = "name";
-    public static final String KEY_LOGIN = "login";
-    public static final String KEY_PASSWORD = "password";
-
-
-    public DBHelper(Context context, String name, int version) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    public DBHelper(Context context) {
+        super(context, "myDB", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_USERS + "(" + KEY_ID +
-                " integer primary key," + KEY_NAME + " text," +
-                KEY_LOGIN + "text," + KEY_PASSWORD + "text" + ")");
+         db.execSQL("create table users ("
+                + "id integer primary key autoincrement,"
+                + "username text,"
+                + "login text,"
+                + "password text" + ");");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists " + TABLE_USERS);
+        db.execSQL("drop table if exists " + "users");
 
         onCreate(db);
     }
--- app/src/main/java/com/example/vika/course_work/SignUpActivity.java
+++ app/src/main/java/com/example/vika/course_work/SignUpActivity.java
@@ -1,13 +1,60 @@
 package com.example.vika.course_work;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends Activity implements View.OnClickListener {
 
+    final String LOG_TAG = "myLogs";
+
+    Button submit;
+    EditText usernameET, loginET, passwordET;
+    DBHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(this);
+        usernameET = (EditText) findViewById(R.id.usernameEditText);
+        loginET = (EditText) findViewById(R.id.loginEditText);
+        passwordET = (EditText) findViewById(R.id.passwordEditText);
+
+        dbHelper = new DBHelper(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String username = usernameET.getText().toString();
+        String login = loginET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        switch (v.getId())
+        {
+            case R.id.submit:
+                contentValues.put("username", username);
+                contentValues.put("login", login);
+                contentValues.put("password", password);
+                long rowID = database.insert("users", null, contentValues);
+                Log.d(LOG_TAG, "row inserted with ID = " + rowID +
+                        " content values : " + contentValues);
+                break;
+        }
+        dbHelper.close();
     }
 }
